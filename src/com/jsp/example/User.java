5f1aee2c3107486a660ba06f8e0e8dd04827a786
shubhadip-raj <shubhadipraj2000@gmail.com>
2023-12-14 13:46:15 +0530

    user details initialise

--- src/com/jsp/example/User.java
+++ src/com/jsp/example/User.java
@@ -3,7 +3,25 @@ package com.jsp.example;
 public class User {
 	private int userID;
 	private String UserName;
-	private String userEmail; 
+	private String userEmail;
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	} 
 	
 
 }
